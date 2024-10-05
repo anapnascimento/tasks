8bf280f90b23ad25fe2a4a4ef3a56d106b957a58
Ana Paula Souza do Nascimento <anapaula.uab@gmail.com>
2024-10-05 16:45:06 -0300

    listando as tarefas

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../../Task';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from "../task-item/task-item.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TaskItemComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  list_tasks: Task[] = [];
 
+  constructor(private taskService:TaskService){}
+  ngOnInit():void{
+    this.taskService.getTasks().subscribe((data) =>{
+      this.list_tasks = data;
+      console.log(data);
+    });
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../../Task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private urlApi = 'http://localhost:3000/tasks'
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.urlApi);
+  }
 }
