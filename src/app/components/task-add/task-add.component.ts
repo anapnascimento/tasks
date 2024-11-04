293ed4741f4fa9fe37e84da8d0b1aa1dde8ba4e8
Ana Paula Souza do Nascimento <anapaula.uab@gmail.com>
2024-11-04 18:02:09 -0300

    Adicionado Form para task
    Adicionado Formulário para cadastrar task

--- src/app/components/task-add/task-add.component.ts
+++ src/app/components/task-add/task-add.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-add',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-add.component.html',
   styleUrl: './task-add.component.css'
 })
 export class TaskAddComponent {
+
+  title_task: string = '';
+  category: string = '';
+  completion: boolean = false;
+
+  onSubmit(){
+
+  }
 
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,11 +3,12 @@ import { TaskService } from '../../services/task.service';
 import { Task } from '../../../Task';
 import { CommonModule } from '@angular/common';
 import { TaskItemComponent } from "../task-item/task-item.component";
+import { TaskAddComponent } from "../task-add/task-add.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, TaskItemComponent],
+  imports: [CommonModule, TaskItemComponent, TaskAddComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
